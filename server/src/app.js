13e953c629bce48619a9f26f3c7299021378174a
AdrianFdz19 <126997909+AdrianFdz19@users.noreply.github.com>
2025-09-17 22:13:23 -0600

    Prueba testear conexion servidor - base de datos aprobada

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { pool } from './config/databaseConfig.js';
 
 export const app = express();
 
@@ -23,6 +24,29 @@ app.get('/test', (req, res) => {
                 status: 200,
                 message,
                 success: true
+            });
+    } catch (err) {
+        console.error(err);
+        res.status(500)
+            .json({
+                status: 500,
+                message: 'Problema en el servidor',
+                success: false
+            });
+    }
+});
+
+/* Api Test conexion servidor - base de datos */
+app.get('/database-test', async (req, res) => {
+    try {
+        const response = await pool.query('SELECT * FROM NOW()');
+        const data = response.rows;
+
+        res.status(200)
+            .json({
+                status: 200,
+                success: true,
+                data
             });
     } catch (err) {
         console.error(err);
